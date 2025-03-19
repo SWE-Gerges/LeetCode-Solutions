70179075b20c5275fe0bdf1294014ee64bfc2eb5
SWE-Gerges <swe.gerges@gmail.com>
2025-03-19 06:12:41 +0200

    Solution Structure

---
+++ LeetCodeConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Running LeetCode Solutions...");
+        Console.WriteLine("HelloWorld!");
+        // Run specific problem solutions
+        // TwoSum twoSum = new TwoSum();
+        // var result = twoSum.Solve(new int[] {2, 7, 11, 15}, 9);
+        // Console.WriteLine($"Two Sum result: [{string.Join(", ", result)}]");
+
+        // Add more calls to other solutions here
+    }
+}
